e81717ea0098925496e2ce1375e400aa67668b33
Ilya Kirbizhekov <ilyoniss@mail.ru>
2023-12-23 20:44:16 +1100

    inital commit

--- src/main/java/com/javarush/jira/common/internal/config/AppProperties.java
+++ src/main/java/com/javarush/jira/common/internal/config/AppProperties.java
@@ -14,6 +14,7 @@ import java.time.Duration;
 @Setter
 public class AppProperties {
 
+
     /**
      * Host url
      */
@@ -41,4 +42,6 @@ public class AppProperties {
         int corePoolSize;
         int maxPoolSize;
     }
+
+
 }
