2221b06e662e0e5c6a1003eefecbcedb709acc9f
Ilya Kirbizhekov <ilyoniss@mail.ru>
2023-12-23 23:39:56 +1100

    inital commit

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,8 +1,20 @@
 package com.javarush.jira.profile.internal.web;
 
 import com.javarush.jira.AbstractControllerTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 class ProfileRestControllerTest extends AbstractControllerTest {
+
+
+    @Test
+    void getUnAuth() throws Exception {
+
+
+    }
+
 
 }
